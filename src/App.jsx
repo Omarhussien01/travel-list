5d46d32cde1c10781942f8d46f2409989acb5c84
HomePC <omar.metwally231@gmail.com>
2023-08-26 16:16:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Header from "./componenets/Header";
+import Form from "./componenets/Form";
+import PackingList from "./componenets/PackingList";
+import Stats from "./componenets/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/componenets/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return (
+        <h1>🤿 Far Away 🏝</h1>
+    )
+}
